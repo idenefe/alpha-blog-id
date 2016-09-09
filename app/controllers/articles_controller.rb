12cde4812d90a34e077a1e52b88d1a52d8d8759e
Rails Udemy Course ID <idenefe@gmail.com>
2016-09-09 02:37:44 +0000

    "added edit and show UI"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@ class ArticlesController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
+  def index
+    @arts = Article.all
+  end
   def new
     @article = Article.new
   end
@@ -17,9 +20,22 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+     @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] ="Article was updated"
+      render 'edit'
+    else
+      render 'edit'
+    end
+  end
   def show
     @article = Article.find(params[:id])
   end
+  
   private
     def article_params
       params.require(:article).permit(:title,:description) 
