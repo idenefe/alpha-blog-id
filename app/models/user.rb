2eaec58e1fea854b925f606327d0cbf4f612e779
Rails Udemy Course ID <idenefe@gmail.com>
2016-09-16 22:40:35 +0000

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
  # validates :title, presence: true,length: {minimum:3, maximum:50}
   #validates :description, presence: true
+  validates :username, 
+    presence: true, 
+    #uniqueness: true, # will not duplicates but is case sensitive (joe!=Joe)
+    uniqueness:{case_sensitive: false}, # will not allow duplicates regardless of case (joe==Joe)
+    length: {minimum:3,maximum:25}
+  
 end
