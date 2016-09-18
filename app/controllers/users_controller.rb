d7fcac8e3a4b9c5aae4a5f1fa9b0d24721b1a7c0
Rails Udemy Course ID <idenefe@gmail.com>
2016-09-18 20:58:18 +0000

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,12 @@
 class UsersController < ApplicationController
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
   before_action :set_user, only: [:edit,:update,:show,:destroy] 
   def new
     @user=User.new
   end
   def create
-    #render plain: params[:user].inspect #this will display a hash representation of the passed data
-    #must whitelist incoming data 
     #debugger # will add breakpoint for byebug debugger
-    @user = User.new(user_params) # calls the private article_params white-listing function
-    if @user.save #will cause error without redirect
+    @user = User.new(user_params)
+    if @user.save #will cause error without redirect unless create view is defined
       flash[:success] ="User was successfully created"
       redirect_to root_path
     else
