b3e54afe66c44fb802474d81e5127880bced95d1
Rails Udemy Course ID <idenefe@gmail.com>
2016-09-18 22:16:34 +0000

    added userpage with gravitar helper, changed and partialized listing of articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80}) # default size of 80 unless size specified in call
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # md5 encrypt email
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}" # url of image on gravatar
+    # return an imagetag (or username if no image found for user)
+    image_tag(gravatar_url, alt: user.username, class: "img-circle") 
+  end
 end
